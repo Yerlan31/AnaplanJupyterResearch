670591508a79cab70f7203189aacf1c88e7f326b
Yerlan31 <mukhanov-yerzhan@mail.ru>
2022-05-15 09:44:33 +0300

    JupyterWorkingFine

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ from prometheus_pandas import query as pp
 import runipy
 import requests
 import json
+import time
 
 
-def promquery(name):
+def promquery(starttime, stoptime):
+
+    time.sleep(5)
     p = query.Prometheus('http://localhost:8428')
     # требуется проверка времени, тк часовые пояса иногда слетают - лучше вывести за день
 
@@ -34,7 +37,7 @@ def promquery(name):
     ############СЕТЬ############
     ram = p.query_range(
         'sum(node_network_receive_bytes_total)',
-        '2022-05-14T01:01:14Z', '2022-05-15T01:01:14Z', '1m')
+        starttime, stoptime, '1s')
     print(ram.to_string())
     with open("metrics_network.txt", "w") as metrics:
         print(ram.to_string(), file=metrics)
@@ -46,15 +49,19 @@ def jupyterstart():
     url = "http://127.0.0.1:9228/starttime"
     r = requests.get(url)
     data = r.json()
-    # jsonData = json.loads(data)
-    # print(jsonData["'text/plain'"])
-    resultstart = (data["text/plain"])
+    resultstart = (data["text/plain"].replace("'", ""))
+
+    # запуск скрипта по блокам
+    for i in range(19):
+        url = "http://127.0.0.1:9228/run%s" % i
+        # print(url)
+        r = requests.get(url)
 
     # получение времени конца запуска
     url = "http://127.0.0.1:9228/stoptime"
     r = requests.get(url)
     data = r.json()
-    resultstop = (data["text/plain"])
+    resultstop = (data["text/plain"].replace("'", ""))
 
     # рабочий курл
     #curl http://localhost:8888/api/contents -H "Authorization: token f4e4170fc9dcae27e1b26aab49492c55e6089ed6fc5ea7da"
@@ -70,6 +77,7 @@ if __name__ == '__main__':
 
     # вручную запускаем ноутбук - запрос на получение время работы ноутбука
     timetuple = jupyterstart()
+    print(timetuple[0], timetuple[1])
     # статистика
-    # promquery('PyCharm')
+    promquery(timetuple[0], timetuple[1])
     # создание временного ряда
